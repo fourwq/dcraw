d0813ad8bb5cc18fbd80eca360ca7c1f06723e41
dcoffin <dcoffin>
2003-10-14 01:16:41 +0000

    Changed canon_has_lowbits() to get 16k instead of 8k.

--- decompress.c
+++ decompress.c
@@ -102,12 +102,12 @@ void parse (int offset, int length)
  */
 int canon_has_lowbits()
 {
-  uchar test[8192];
+  uchar test[0x4000];
   int ret=1, i;
 
   fseek (ifp, 0, SEEK_SET);
-  fread (test, 1, 8192, ifp);
-  for (i=540; i < 8191; i++)
+  fread (test, 1, sizeof test, ifp);
+  for (i=540; i < sizeof test - 1; i++)
     if (test[i] == 0xff) {
       if (test[i+1]) return 1;
       ret=0;
