28214351fdbb399fe869026055ed2a1c357cad38
dcoffin <dcoffin>
2004-05-01 04:08:26 +0000

    Fixed Kodak YUV code for widths not divisible by 8.

--- parse.c
+++ parse.c
@@ -479,6 +479,11 @@ void kodak_yuv_decode (FILE *tfp)
 	  blen[i++] = c >> 4;
 	}
 	li = bitbuf = bits = y[1] = y[3] = cb = cr = 0;
+	if (len % 8 == 4) {
+	  bitbuf  = fgetc(ifp) << 8;
+	  bitbuf += fgetc(ifp);
+	  bits = 16;
+	}
       }
       for (si=0; si < 6; si++) {
 	len = blen[li++];
